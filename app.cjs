ab8674d82b4b37aa6554805be9b3a797050b2c35
Cookie <alstrokash@gmail.com>
2024-05-27 22:56:33 +0300

    added portraits

--- app.cjs
+++ app.cjs
@@ -22,6 +22,12 @@ app.get("/spells", (req, res) => {
   res.send(require("./src/spells.cjs"));
 });
 
+app.get("/<race>-<class>-<sex>", (req, res) => {
+  res.send(
+    require(`./src/portraits/${req.params.race}-${req.params.class}-${req.params.sex}.jpg`)
+  );
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
