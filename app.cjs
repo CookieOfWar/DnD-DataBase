e4e861bd4114e5aabed717ca877c6884f02a11be
Cookie <alstrokash@gmail.com>
2024-05-27 16:52:13 +0300

    added CORS

--- app.cjs
+++ app.cjs
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 port = 8000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("DnD-API");
